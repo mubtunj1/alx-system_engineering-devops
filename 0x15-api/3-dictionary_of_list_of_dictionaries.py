f7250c8408093c37a502c838443917a580d0b7f3
Mubtunj <mubtunji01@gmail.com>
2023-05-23 03:43:51 +0100

    Task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Accessing todo list"""
+import json
 import requests
 import sys
-import json
+
 if __name__ == "__main__":
 
     baseUrl = "https://jsonplaceholder.typicode.com/users"
